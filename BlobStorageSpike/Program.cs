09595079a68818e313a08c142d5cbe37a74a675c
Sheila Coles <sheila.coles@ukho.gov.uk>
2023-05-22 12:21:07 +0100

    Add project files.

---
+++ BlobStorageSpike/Program.cs
@@ -0,0 +1,41 @@
+﻿using Azure.Storage;
+using Azure.Storage.Blobs;
+
+const string blobContainerName = "blobbercontainer";
+
+string connectionString = "";
+
+var blobServiceClient = new BlobServiceClient(connectionString);
+
+var blobContainerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
+
+// Create a local file in the ./data/ directory for uploading and downloading
+string localPath = "data";
+Directory.CreateDirectory(localPath);
+string fileName = "quickstart" + Guid.NewGuid().ToString() + ".txt";
+string localFilePath = Path.Combine(localPath, fileName);
+
+// Write text to the file
+await File.WriteAllTextAsync(localFilePath, "Hello, Mazu!");
+
+// Get a reference to a blob
+BlobClient blobClient = blobContainerClient.GetBlobClient(fileName);
+
+// Generate SAS Token 
+Azure.Storage.Sas.BlobSasBuilder blobSasBuilder = new Azure.Storage.Sas.BlobSasBuilder()
+{
+    BlobContainerName = blobContainerName,
+    BlobName = fileName,
+    ExpiresOn = DateTime.UtcNow.AddMinutes(5)
+};
+blobSasBuilder.SetPermissions(Azure.Storage.Sas.BlobSasPermissions.Read);
+var azureStorageAccount = "blobbercontainer";
+var azureStorageAccessKey = "";
+var sasToken = blobSasBuilder.ToSasQueryParameters(new StorageSharedKeyCredential(azureStorageAccount, azureStorageAccessKey)).ToString();
+
+// Log link to uploaded content
+var link = $"{blobClient.Uri}?{sasToken}";
+Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", link);
+
+// Upload data from the local file
+await blobClient.UploadAsync(localFilePath, false);
